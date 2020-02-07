dce177e13ac417dcd951c6cc48256f7e97eb838b
dingqiuwen <dqw17812538094@163.com>
2020-02-07 14:34:29 +0800

    add

---
+++ Food/src/components/Head.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="head">
+    <div class="headSearch">
+      <img src="../../static/images/logo.png" alt="">
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Head',
+  data () {
+    return {
+      
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style lang="stylus" scoped>
+  .headSearch{
+    width 980px
+    height 90px
+    margin 0 auto 
+  }
+
+</style>
